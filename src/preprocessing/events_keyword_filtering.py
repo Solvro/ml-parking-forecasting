7584fa4db0cbbcb92b2a2e1d36ff61bf35a0f394
DominikaStefaniak <161628339+DominikaStefaniak@users.noreply.github.com>
2026-02-23 17:55:53 +0100

    fix: compile regex patterns

--- src/preprocessing/events_keyword_filtering.py
+++ src/preprocessing/events_keyword_filtering.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import re
+
 location_regex_list = [
     r"\b[a-h]-?\d{1,2}\.?\d?\b",
     r"\bsks\b",
@@ -12,17 +15,20 @@ location_regex_list = [
     r"geocentrum"
 ]
 
-location_combined_regex = "|".join(location_regex_list)
+location_pattern = re.compile(
+    "|".join(location_regex_list),
+    re.IGNORECASE,
+)
 
-
-def filter_event_locations(df_events, location_regex=location_combined_regex):
+def filter_event_locations(df_events, pattern=location_pattern, copy: bool = False):
     """Creates a boolean column "location_filtered" in the input dataframe df_events,
     indicating whether the "location" column matches any of the specified regex patterns."""
 
-    df_events = df_events.copy()
-    df_events["location_filtered"] = df_events["location"].str.contains(
-        location_regex, case=False, regex=True, na=False
-    )
+    if copy:
+        df_events = df_events.copy()
+
+    df_events["is_location_filtered"] = df_events["location"].str.contains(pattern, na=False)
+
     return df_events
 
 
@@ -37,45 +43,47 @@ summary_regex_list = [
     r"spotkanie choinkowe",
     r"among pwr"]
 
-summary_combined_regex = "|".join(summary_regex_list)
+summary_pattern = re.compile(
+    "|".join(summary_regex_list),
+    re.IGNORECASE,
+)
 
 
-def filter_event_summaries(df_events, summary_regex=summary_combined_regex):
+def filter_event_summaries(df_events, pattern=summary_pattern, copy: bool = False):
     """Creates a boolean column "summary_filtered" in the input dataframe df_events,
     indicating whether the "summary" column matches any of the specified regex patterns."""
 
-    df_events = df_events.copy()
-    df_events["summary_filtered"] = df_events["summary"].str.contains(
-        summary_regex, case=False, regex=True, na=False
-    )
+    if copy:
+        df_events = df_events.copy()
+    df_events["is_summary_filtered"] = df_events["summary"].str.contains(pattern, na=False)
     return df_events
 
 
-def filter_events(df_events, location_regex=location_combined_regex, summary_regex=summary_combined_regex):
+def filter_events(df_events, location_pattern=location_pattern, summary_pattern=summary_pattern, copy: bool = False):
     """
     Creates a single boolean column 'filter_events' which is True
     if either location or summary matches the specified regex patterns.
     Drops intermediate helper columns.
     """
-    df_events = df_events.copy()
 
-    df_events = filter_event_locations(df_events, location_regex)
-    df_events = filter_event_summaries(df_events, summary_regex)
+    if copy:
+        df_events = df_events.copy()
 
-    df_events["filter_events"] = (df_events["location_filtered"] | df_events["summary_filtered"])
+    df_events = filter_event_locations(df_events, location_pattern)
+    df_events = filter_event_summaries(df_events, summary_pattern)
 
-    df_events = df_events.drop(columns=["location_filtered", "summary_filtered"])
+    df_events["is_filtered_event"] = (df_events["is_location_filtered"] | df_events["is_summary_filtered"])
+
+    df_events = df_events.drop(columns=["is_location_filtered", "is_summary_filtered"])
 
     return df_events
 
 
 if __name__ == "__main__":
-    import pandas as pd
-
     df_events = pd.read_parquet('data/pwr_events.parquet')
     #df_filtered_locations = filter_event_locations(df_events)
     #df_filtered = filter_event_summaries(df_filtered_locations)
-    #print(df_filtered[["location", "location_filtered", "summary", "summary_filtered"]].head(20))
+    #print(df_filtered[["location", "is_location_filtered", "summary", "is_summary_filtered"]].head(20))
 
     df_filtered = filter_events(df_events)
-    print(df_filtered[["location", "summary", "filter_events"]].head(20))
+    print(df_filtered[["location", "summary", "is_filtered_event"]].head(20))
